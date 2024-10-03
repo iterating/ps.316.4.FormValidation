b222529bffa6946ec8b85affd8645180bbdd2fb6
Jonathan Young <jy>
2024-10-02 19:11:33 -0700

    [x] password

--- 316.4.FormValidation.js
+++ 316.4.FormValidation.js
@@ -1,6 +1,5 @@
 const registrationForm = document.getElementById("registration");
 
-let errors = [];
 function showErrors(errors) {
   const errorDisplay = document.getElementById("errorDisplay");
   errorDisplay.innerHTML = errors.join("<br>");
@@ -8,6 +7,7 @@ function showErrors(errors) {
 }
 registrationForm.addEventListener("submit", function (e) {
   e.preventDefault();
+  let errors = [];
 
   try {
     // Make a object from the form
@@ -16,6 +16,9 @@ registrationForm.addEventListener("submit", function (e) {
     // The username must be at least four characters long.
     // The username must contain at least two unique characters.
     // The username cannot contain any special characters or whitespace.
+    if (!formData.get("username")) {
+      errors.push("The username cannot be blank.");
+    }
     if (formData.get("username").length < 4) {
       errors.push("The username must be at least four characters long.");
     }
@@ -31,20 +34,42 @@ registrationForm.addEventListener("submit", function (e) {
     // Registration Form - Email Validation:
     // The email must be a valid email address.
     // The email must not be from the domain "example.com."
-    if (/\S+@\S+\.\S+/.test(formData.get("email"))) {
-      errors.push("The email must not be from the domain (example.com)");
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(formData.get("email"))) {
+      errors.push("The email must be a valid email address.");
     }
+    if (formData.get("email").endsWith("example.com")) {
+        errors.push("The email must not be from the domain example.com");
+      }
+
+      // Registration Form - Password Validation:
+      // Passwords must be at least 12 characters long.
+      // Passwords must have at least one uppercase and one lowercase letter.
+      // Passwords must contain at least one number.
+      // Passwords must contain at least one special character.
+      // Passwords cannot contain the word "password" (uppercase, lowercase, or mixed).
+      // Passwords cannot contain the username.
+      // Both passwords must match.
+    let password = formData.get("password")
     if (formData.get("password").length < 12) {
       errors.push("Passwords must be at least 12 characters long.");
     }
-    // Registration Form - Password Validation:
-    // Passwords must be at least 12 characters long.
-    // Passwords must have at least one uppercase and one lowercase letter.
-    // Passwords must contain at least one number.
-    // Passwords must contain at least one special character.
-    // Passwords cannot contain the word "password" (uppercase, lowercase, or mixed).
-    // Passwords cannot contain the username.
-    // Both passwords must match.
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]).+$/;
+    if (!passwordRegex.test.password) {
+        errors.push("The password must contain at least one uppercase and one lowercase letter.");
+    }
+    if (!/[0-9]/.test.password){
+        errors.push("Passwords must contain at least one number.");
+    }
+    if (!/[^\w\s]/.test.password){
+        errors.push("Passwords must contain at least one special character");
+    }
+        if (password.includes(username)){
+        errors.push("Passwords must contain at least one special character");
+    }
+        
+
+
   } catch (error) {
     errorDisplay.textContent = "An error occured during form submission";
   }
@@ -61,7 +86,7 @@ registrationForm.addEventListener("submit", function (e) {
 // Registration Form - Form Submission:
 // Usually, we would send this information to an external API for processing. In our case, we are going to process and store the data locally for practice purposes.
 // If all validation is successful, store the username, email, and password using localStorage.
-// If you are unfamiliar with localStorage, that is okay! Reference the documentation's "Description" and "Examples" sections to learn how to implement it. If you run into issues speak with a peer or one of your instructors.
+// If you are unfamiliar with localSttorage, that is okay! Reference the documentation's "Description" and "Examples" sections to learn how to implement it. If you run into issues speak with a peer or one of your instructors.
 // Consider how you want to store the user data, keeping in mind that there will be quite a few users registering for the site. Perhaps you want to store it with an array of user objects; or maybe an object whose keys are the usernames themselves.
 // Valid usernames should be converted to all lowercase before being stored.
 // Valid emails should be converted to all lowercase before being stored.
